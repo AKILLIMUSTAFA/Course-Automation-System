00fa7f08d9dd52f2753a096f170265f001f63452
MUSTAFA AKILLI <makilli@gtu.edu.tr>
2016-02-24 14:44:17 +0200

    Added class WhiteboardDescription

---
+++ src/main/java/tr/edu/gtu/mustafa/akilli/Document/WhiteboardDescription.java
@@ -0,0 +1,24 @@
+package tr.edu.gtu.mustafa.akilli.Document;
+
+/**
+ * HW01_131044017_Mustafa_Akilli
+ *
+ * File:   WhiteboardDescription
+ *
+ * Description:
+ *
+ * WhiteboardDescription extends AbstractDocument
+ *
+ * @author Mustafa_Akilli
+ * @since Wednesday 24 February 2016, 14:40 by Mustafa_Akilli
+ */
+public class WhiteboardDescription extends AbstractDocument{
+    /**
+     * WhiteboardDescription one parameters constructor
+     *
+     * @param newWhiteboardDescriptionName new WhiteboardDescription's Name
+     */
+    public WhiteboardDescription(String newWhiteboardDescriptionName) {
+        super(newWhiteboardDescriptionName);
+    }
+}
