24538a5268a99cca6ad2599bb7c8d7fefe80f4c3
MUSTAFA AKILLI <makilli@gtu.edu.tr>
2016-02-24 14:37:24 +0200

    Added class Slide

---
+++ src/main/java/tr/edu/gtu/mustafa/akilli/Document/Slide.java
@@ -0,0 +1,24 @@
+package tr.edu.gtu.mustafa.akilli.Document;
+
+/**
+ * HW01_131044017_Mustafa_Akilli
+ *
+ * File:   Slide
+ *
+ * Description:
+ *
+ * Slide extends AbstractDocument
+ *
+ * @author Mustafa_Akilli
+ * @since Wednesday 24 February 2016, 14:37 by Mustafa_Akilli
+ */
+public class Slide extends AbstractDocument{
+    /**
+     * AbstractSlide one parameters constructor
+     *
+     * @param newSlideName new Slide's Name
+     */
+    public Slide(String newSlideName) {
+        super(newSlideName);
+    }
+}
